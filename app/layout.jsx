68bd76709d2bad979bbba909c42f6b65524d864d
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-09-03 09:39:21 +0200

    Implement header and start dashboard

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <Header />
           <main className="container">
             <div className="flex items-start justify-center min-h-screen">
               <div className="mt-20">{children}</div>
