e8e3309e61ab5bc57f9de41a9b9ed1d049db4dfa
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-09-03 12:03:18 +0200

    Add conditional header links

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { auth } from "@clerk/nextjs";
 
 const Header = () => {
+  const { userId } = auth();
+
   return (
     <>
       <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
@@ -11,13 +14,23 @@ const Header = () => {
             </div>
           </Link>
         </div>
-        <div className="text-white">
-          <Link href="sign-in" className="text-gray-300 hover:text-white mr-4">
-            Sign In
-          </Link>
-          <Link href="sign-up" className="text-gray-300 hover:text-white mr-4">
-            Sign Up
-          </Link>
+        <div className="text-white flex items-center">
+          {!userId && (
+            <>
+              <Link
+                href="sign-in"
+                className="text-gray-300 hover:text-white mr-4"
+              >
+                Sign In
+              </Link>
+              <Link
+                href="sign-up"
+                className="text-gray-300 hover:text-white mr-4"
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
         </div>
       </nav>
     </>
